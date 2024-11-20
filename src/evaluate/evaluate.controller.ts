1af2496d4f5c50c892c3b5e3a7724261db95e841
PedrooCamilo <pedro2005camilo@gmail.com>
2024-11-20 16:18:25 -0300

    feat(evaluate): CRUD completa com validações para modelo de avaliação

--- src/evaluate/evaluate.controller.ts
+++ src/evaluate/evaluate.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, ParseUUIDPipe, ValidationPipe } from '@nestjs/common';
 import { EvaluateService } from './evaluate.service';
+import { CreateEvaluateDto } from './dto/create-evaluate.dto';
+import { UpdateEvaluateDto } from './dto/update-evaluate.dto';
 
 
 
@@ -7,9 +9,24 @@ import { EvaluateService } from './evaluate.service';
 export class EvaluateController {
     constructor(private readonly evaluateService: EvaluateService) {}
 
+    @Post()
+    create(@Body() createEvaluateDto: CreateEvaluateDto){
+        return this.evaluateService.create(createEvaluateDto);
+    }
+
     @Get()
     findAll() {
         return this.evaluateService.findAll();
+    }
+    
+    @Patch(':id')
+    update(@Param('id', ParseUUIDPipe) id: string, @Body(ValidationPipe) updateEvaluateDto: UpdateEvaluateDto) {
+        return this.evaluateService.update(id, updateEvaluateDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseUUIDPipe) id: string) {
+        return this.evaluateService.remove(id);
     }
 }
 
